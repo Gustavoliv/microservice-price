4fbccaceb37a28f055ab61a6ace942a3facf2004
Gustavoliv <gustavo.olivaranchal@gmail.com>
2024-02-20 21:55:31 +0100

    Mappings adapts to java 11

--- src/main/java/com/inditex/ecommerce/prices/infraestructure/rest/mapper/PriceMapper.java
+++ src/main/java/com/inditex/ecommerce/prices/infraestructure/rest/mapper/PriceMapper.java
@@ -2,7 +2,6 @@ package com.inditex.ecommerce.prices.infraestructure.rest.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 import org.springframework.stereotype.Component;
 
 import com.inditex.ecommerce.prices.domain.model.Price;
@@ -12,15 +11,13 @@ import com.inditex.ecommerce.prices.infraestructure.entity.PriceEntity;
 @Mapper(componentModel = "spring")
 public interface PriceMapper {
     
-    @Mappings({
-        @Mapping(source = "brandId", target = "brand.brandId"),
-        @Mapping(source = "startDate", target = "startDate"),
-        @Mapping(source = "endDate", target = "endDate"),
-        @Mapping(source = "priceList", target = "priceList"),
-        @Mapping(source = "productId", target = "product.productId"),
-        @Mapping(source = "priority", target = "priority"),
-        @Mapping(source = "price", target = "price"),
-        @Mapping(source = "curr", target = "curr")
-    })
+    @Mapping(source = "brandId", target = "brand.brandId")
+    @Mapping(source = "startDate", target = "startDate")
+    @Mapping(source = "endDate", target = "endDate")
+    @Mapping(source = "priceList", target = "priceList")
+    @Mapping(source = "productId", target = "product.productId")
+    @Mapping(source = "priority", target = "priority")
+    @Mapping(source = "price", target = "price")
+    @Mapping(source = "curr", target = "curr")
     Price toPrice(PriceEntity priceEntity);
 }
