a506dab814b9bbe069aa92e27b9c800c27777b48
Gustavoliv <gustavo.olivaranchal@gmail.com>
2024-02-21 13:41:07 +0100

    New constants

--- src/main/java/com/inditex/ecommerce/prices/infraestructure/rest/constants/Constant.java
+++ src/main/java/com/inditex/ecommerce/prices/infraestructure/rest/constants/Constant.java
@@ -7,7 +7,11 @@ public class Constant {
 
     public static final String FORMAT_DATE          = "yyyy-MM-dd'T'HH:mm:ss";
     public static final String EMPTY_PRODUCT_FIELD  = "'productId' field is required";
-    public static final String EMPTY_BRAND_FIELD  = "'brandId' field is required";
+    public static final String EMPTY_BRAND_FIELD    = "'brandId' field is required";
     public static final String FORMAT_PRODUCT       = "35444";
     public static final String FORMAT_BRAND         = "1";
+    public static final String PRICE_NOT_FOUND      = "The price was not found for the specified parameters";
+    public static final String HTTP_INTERNAL_ERROR  = "Internal Server Error";
+    public static final String INCORRECT_PARAM      = "INCORRECT_PARAMETER";
+
 }
