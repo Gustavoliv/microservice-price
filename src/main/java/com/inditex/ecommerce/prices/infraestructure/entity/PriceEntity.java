069eb08416aa435361f3107c474739fc5caacb7c
Gustavoliv <gustavo.olivaranchal@gmail.com>
2024-02-21 11:29:56 +0100

    Buider is added

--- src/main/java/com/inditex/ecommerce/prices/infraestructure/entity/PriceEntity.java
+++ src/main/java/com/inditex/ecommerce/prices/infraestructure/entity/PriceEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +17,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 @Table(name = "PRICES")
 public class PriceEntity {
     
