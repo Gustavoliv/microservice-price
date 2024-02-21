0faa1362e1ebd470185fd0ba0a6644daab4e92e8
Gustavoliv <gustavo.olivaranchal@gmail.com>
2024-02-21 09:26:41 +0100

    test is added

--- src/test/java/com/inditex/ecommerce/prices/domain/PriceServiceTests.java
+++ src/test/java/com/inditex/ecommerce/prices/domain/PriceServiceTests.java
@@ -1,161 +1,59 @@
 package com.inditex.ecommerce.prices.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.inditex.ecommerce.prices.application.mapper.PriceDtoMapper;
 import com.inditex.ecommerce.prices.application.service.PriceServiceImpl;
 import com.inditex.ecommerce.prices.domain.model.Brand;
 import com.inditex.ecommerce.prices.domain.model.Price;
 import com.inditex.ecommerce.prices.domain.model.Product;
 import com.inditex.ecommerce.prices.domain.model.dto.PriceDTO;
+import com.inditex.ecommerce.prices.domain.port.in.PriceDomainRepository;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 class PriceServiceTests {
         
-        @Autowired
+        @Mock
+        private PriceDomainRepository priceDomainRepository;
+        
+        @Mock
+        private PriceDtoMapper priceDtoMapper;
+        
+        @InjectMocks
         private PriceServiceImpl priceService;
         
-        @Test
-        void servicePrice_whenExists_shouldReturnPriceTest01() {
-
-                Price searchPrice = Price.builder()
-                        .startDate(LocalDateTime.of(2020, 6, 14, 10, 0, 0))
-                        .endDate(LocalDateTime.of(2020, 6, 14, 10, 0,0))
-                        .brand(new Brand(1))
-                        .product(new Product(35455L))
-                        .build();
-
-                PriceDTO expectedPriceDto = PriceDTO.builder()
-                        .brandId(1)
-                        .productId(35455L)
-                        .startDate(LocalDateTime.of(2020, 6, 14, 0, 0, 0))
-                        .endDate(LocalDateTime.of(2020, 12, 31, 23, 59,59))
-                        .priceList(1L)
-                        .price(BigDecimal.valueOf(35.50).setScale(2, RoundingMode.HALF_UP))
-                        .build();
-
-                Optional<PriceDTO> result = priceService.getPriceByBrandProductDate(searchPrice);
-
-                assertTrue(result.isPresent());
-                assertEquals(result.get(), expectedPriceDto);
+        @BeforeEach
+        public void setUp() {
+                MockitoAnnotations.openMocks(this);
         }
         
         @Test
-        void servicePrice_whenExists_shouldReturnPriceTest02() {
-
-                Price searchPrice = Price.builder()
-                        .startDate(LocalDateTime.of(2020, 6, 14, 16, 0, 0))
-                        .endDate(LocalDateTime.of(2020, 6, 14, 16, 0,0))
-                        .brand(new Brand(1))
-                        .product(new Product(35455L))
-                        .build();
-
-                PriceDTO expectedPriceDto = PriceDTO.builder()
-                        .brandId(1)
-                        .productId(35455L)
-                        .startDate(LocalDateTime.of(2020, 6, 14, 15, 0, 0))
-                        .endDate(LocalDateTime.of(2020, 06, 14, 18, 30,0))
-                        .priceList(2L)
-                        .price(BigDecimal.valueOf(25.45).setScale(2, RoundingMode.HALF_UP))
-                        .build();
-
-                Optional<PriceDTO> result = priceService.getPriceByBrandProductDate(searchPrice);
-
-                assertTrue(result.isPresent());
-                assertEquals(result.get(), expectedPriceDto);
-        }
-        
-        @Test
-        void servicePrice_whenExists_shouldReturnPriceTest03() {
-
-                Price searchPrice = Price.builder()
-                        .startDate(LocalDateTime.of(2020, 6, 14, 21, 0, 0))
-                        .endDate(LocalDateTime.of(2020, 6, 14, 21, 0,0))
-                        .brand(new Brand(1))
-                        .product(new Product(35455L))
-                        .build();
-
-                PriceDTO expectedPriceDto = PriceDTO.builder()
-                        .brandId(1)
-                        .productId(35455L)
-                        .startDate(LocalDateTime.of(2020, 6, 14, 0, 0, 0))
-                        .endDate(LocalDateTime.of(2020, 12, 31, 23, 59,59))
-                        .priceList(1L)
-                        .price(BigDecimal.valueOf(35.50).setScale(2, RoundingMode.HALF_UP))
-                        .build();
-
-                Optional<PriceDTO> result = priceService.getPriceByBrandProductDate(searchPrice);
-
-                assertTrue(result.isPresent());
-                assertEquals(result.get(), expectedPriceDto);
-        }
-        
-        @Test
-        void servicePrice_whenExists_shouldReturnPriceTest04() {
-
-                Price searchPrice = Price.builder()
-                        .startDate(LocalDateTime.of(2020, 6, 15, 10, 0, 0))
-                        .endDate(LocalDateTime.of(2020, 6, 15, 10, 0,0))
-                        .brand(new Brand(1))
-                        .product(new Product(35455L))
-                        .build();
-
-                PriceDTO expectedPriceDto = PriceDTO.builder()
-                        .brandId(1)
-                        .productId(35455L)
-                        .startDate(LocalDateTime.of(2020, 6, 15, 0, 0, 0))
-                        .endDate(LocalDateTime.of(2020, 6, 15, 11, 0,0))
-                        .priceList(3L)
-                        .price(BigDecimal.valueOf(30.50).setScale(2, RoundingMode.HALF_UP))
-                        .build();
-
-                Optional<PriceDTO> result = priceService.getPriceByBrandProductDate(searchPrice);
-
-                assertTrue(result.isPresent());
-                assertEquals(result.get(), expectedPriceDto);
-        }
-        
-        @Test
-        void servicePrice_whenExists_shouldReturnPriceTest05() {
-
-                Price searchPrice = Price.builder()
-                        .startDate(LocalDateTime.of(2020, 6, 16, 21, 0, 0))
-                        .endDate(LocalDateTime.of(2020, 6, 16, 21, 0,0))
-                        .brand(new Brand(1))
-                        .product(new Product(35455L))
-                        .build();
-
-                PriceDTO expectedPriceDto = PriceDTO.builder()
-                        .brandId(1)
-                        .productId(35455L)
-                        .startDate(LocalDateTime.of(2020, 6, 15, 16, 0, 0))
-                        .endDate(LocalDateTime.of(2020, 12, 31, 23, 59,59))
-                        .priceList(4L)
-                        .price(BigDecimal.valueOf(38.95).setScale(2, RoundingMode.HALF_UP))
-                        .build();
-
-                Optional<PriceDTO> result = priceService.getPriceByBrandProductDate(searchPrice);
-
-                assertTrue(result.isPresent());
-                assertEquals(result.get(), expectedPriceDto);
-        }
-
-        @Test
-        void servicePrice_whenGetSeveralResult_shouldGetHighestPriority() {
+        void testGetPriceByBrandProductDate() {
 
                 Price searchPrice = Price.builder()
                         .startDate(LocalDateTime.of(2020, 6, 14, 17, 0, 0))
                         .endDate(LocalDateTime.of(2020, 6, 14, 17, 0,0))
+                        .brand(new Brand(1))
+                        .product(new Product(35455L))
+                        .build();
+                
+                Price returnPrice = Price.builder()
+                        .startDate(LocalDateTime.of(2020, 6, 14, 15, 0, 0))
+                        .endDate(LocalDateTime.of(2020, 6, 14, 18, 30,0))
                         .brand(new Brand(1))
                         .product(new Product(35455L))
                         .build();
@@ -169,25 +67,14 @@ class PriceServiceTests {
                         .price(BigDecimal.valueOf(25.45).setScale(2, RoundingMode.HALF_UP))
                         .build();
 
-                Optional<PriceDTO> result = priceService.getPriceByBrandProductDate(searchPrice);
-
-                assertTrue(result.isPresent());
-                assertEquals(result.get(), expectedPriceDto);
-        }
-
-        @Test
-        void endpointPrice_whenNotExist_shouldReturnEmpty() {
-
-                Price searchPrice = Price.builder()
-                        .startDate(LocalDateTime.of(2010, 6, 16, 21, 0, 0))
-                        .endDate(LocalDateTime.of(2010, 6, 16, 21, 0,0))
-                        .brand(new Brand(1))
-                        .product(new Product(35455L))
-                        .build();
+                when(priceDomainRepository.getPriceByBrandProductDate(searchPrice)).thenReturn(Optional.of(returnPrice));
+                when(priceDtoMapper.toPriceDto(returnPrice)).thenReturn(expectedPriceDto);
 
                 Optional<PriceDTO> result = priceService.getPriceByBrandProductDate(searchPrice);
 
-                assertTrue(result.isEmpty());
+                assertEquals(expectedPriceDto, result.get());
+                verify(priceDomainRepository, times(1)).getPriceByBrandProductDate(searchPrice);
+                verify(priceDtoMapper, times(1)).toPriceDto(returnPrice);
         }
 }
 
